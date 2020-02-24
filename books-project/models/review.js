e33b0c0c39e6ab1a262910ed013ff15653da808b
Myrian Katto <myriankatto@gmail.com>
2020-02-24 11:16:39 +0000

    review model

---
+++ books-project/models/review.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Books'
+    },
+    author: {
+      type: mongoose.Schema.Type.ObjectId,
+      ref: 'User'
+    },
+    content: {
+      type: String
+    },
+    rate: {
+      type: Number
+    },
+    timestamps: {
+      createdAt: 'creationDate',
+      updatedAt: 'updateDate'
+    }
+  }
+);
+
+module.exports = mongoose.model('Review', reviewSchema);
