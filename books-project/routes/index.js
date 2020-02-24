4856cf0fcb1a5c1b667ec85b37ae3ef89ef048f5
Gabriela Fonseca <gabmfonseca@gmail.com>
2020-02-24 14:31:04 +0000

    api connected

--- books-project/routes/index.js
+++ books-project/routes/index.js
@@ -2,13 +2,29 @@
 
 const { Router } = require('express');
 const router = new Router();
+const axios = require('axios');
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
 });
 
 router.get('/search', (req, res, next) => {
-  //....
- res.render('book/search');
+  const term = req.query.term;
+  // console.log(term);
+
+  const requestPromise = axios.get(`https://www.googleapis.com/books/v1/volumes?q=${term}`);
+
+  requestPromise
+    .then(output => {
+      const data = output.data.items.map(file => {
+        return file.volumeInfo;
+      });
+      //console.log(data);
+      res.render('book/search', { data });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
+
 module.exports = router;
