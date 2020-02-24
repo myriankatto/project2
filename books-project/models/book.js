5aac46db27f0326f16414b68a47dc9697d34da71
Myrian Katto <myriankatto@gmail.com>
2020-02-24 11:01:11 +0000

    book model

---
+++ books-project/models/book.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const booksSchema = new mongoose.Schema({
+  authors: [
+    {
+      type: String,
+      trim: true
+    }
+  ],
+  title: {
+    type: String,
+    trim: true
+  },
+  subtitle: {
+    type: String
+  },
+  year: {
+    type: Number
+  },
+  genre: [
+    {
+      type: String
+    }
+  ],
+  publisher: {
+    type: String
+  },
+  publishedDate: {
+    type: Date
+  },
+  description: {
+    type: String
+  },
+  pageCount: {
+    type: Number
+  },
+  categories: [
+    {
+      type: String
+    }
+  ],
+  imageLinks: {
+    smallThumbnail: {
+      type: String
+    },
+    thumbnail: {
+      type: String
+    }
+  },
+  language: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Books', booksSchema);
+
